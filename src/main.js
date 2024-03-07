4ad9a272bfdb488c74f565e5e59509a235a4a676
Li Peng <kailous@live.cn>
2024-03-07 15:15:36 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/U0qa9sNBRI9QGuMx/scene.splinecode');
